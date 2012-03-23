2c1e6c2830cedcb7b92d501a89b568dd787f2c7b
Taylor Rahm <taylor.rahm2@gmail.com>
2012-03-23 14:39:12 -0500

    changed static root

--- resume_site/urls.py
+++ resume_site/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 import settings
 
 admin.autodiscover()
@@ -7,5 +8,4 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	url(r'^$', 'resumes.views.main'),
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^media/(.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
-)
+) + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
